027e0429bd222c507fb6f77c41d99c646ebf3762
Loïc Seigland <loic@loicseigland.ie>
2014-05-21 12:10:20 +0100

    no message

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,22 +13,22 @@ RSpec.configure do |config|
       c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
       c.allow_http_connections_when_no_cassette = true
       c.hook_into :webmock
-      c.before_record do |i|
+      # c.before_record do |i|
 
-        i.response.headers.delete('Set-Cookie')
-        i.request.headers.delete('Authorization')
+      #   i.response.headers.delete('Set-Cookie')
+      #   i.request.headers.delete('Authorization')
 
-        u = URI.parse(i.request.uri)
-        i.request.uri.sub!(%r{:\/\/.*#{Regexp.escape(u.host)}}, "://#{u.host}")
+      #   u = URI.parse(i.request.uri)
+      #   i.request.uri.sub!(%r{:\/\/.*#{Regexp.escape(u.host)}}, "://#{u.host}")
 
-      end
+      # end
 
       # Matches authenticated requests regardless of their Basic
       # auth string (https://user:pass@domain.tld)
-      c.register_request_matcher :anonymized_uri do |request_1, request_2|
-        (URI(request_1.uri).port == URI(request_2.uri).port) &&
-          URI(request_1.uri).path == URI(request_2.uri).path
-      end
+      # c.register_request_matcher :anonymized_uri do |request_1, request_2|
+      #   (URI(request_1.uri).port == URI(request_2.uri).port) &&
+      #     URI(request_1.uri).path == URI(request_2.uri).path
+      # end
     end
 
   end
