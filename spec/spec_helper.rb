a67a8c1fd8d316ff6afca41d9cfb6e31c24b344f
Loïc Seigland <loic@loicseigland.ie>
2014-05-23 12:53:47 +0100

    Improved specs and API connectors Added travis file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'vcr'
 require 'webmock'
 require 'webmock/rspec'
 require 'factory_girl'
+require 'yaml'
+require 'CGI'
 
 RSpec.configure do |config|
   FactoryGirl.find_definitions
@@ -13,22 +15,16 @@ RSpec.configure do |config|
       c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
       c.allow_http_connections_when_no_cassette = true
       c.hook_into :webmock
-      c.before_record do |i|
 
-        i.response.headers.delete('Set-Cookie')
-        i.request.headers.delete('Authorization')
+      # Created a yml file with credentials 
+      # ignored by git repository
+      credentials = YAML.load_file('spec/fixtures/test_credentials.yml')
+      
+      c.filter_sensitive_data('<USERNAME>') { credentials['username'] }
+      c.filter_sensitive_data('<PASSWORD>') { CGI.escape credentials['password'] }
+      c.filter_sensitive_data('<PASSWORD>') { credentials['password'] }
 
-        u = URI.parse(i.request.uri)
-        i.request.uri.sub!(%r{:\/\/.*#{Regexp.escape(u.host)}}, "://#{u.host}")
-
-      end
-
-      # Matches authenticated requests regardless of their Basic
-      # auth string (https://user:pass@domain.tld)
-      c.register_request_matcher :anonymized_uri do |request_1, request_2|
-        (URI(request_1.uri).port == URI(request_2.uri).port) &&
-          URI(request_1.uri).path == URI(request_2.uri).path
-      end
+      
     end
 
   end
